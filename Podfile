93bba0779c2fb50b35c31c81f7217405101fe41a
chrisabbod <chrisabbod@gmail.com>
2019-01-28 15:51:13 -0500

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'WeScanExample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WeScanExample
+    pod 'WeScan', '>= 0.9'
+
+  target 'WeScanExampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WeScanExampleUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ WeScanExample/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  WeScanExample
+//
+//  Created by Chris Abbod on 1/28/19.
+//  Copyright © 2019 Chris Abbod. All rights reserved.
+//
+
+import UIKit
+import WeScan
+
+class ViewController: UIViewController, ImageScannerControllerDelegate {
+
+    @IBOutlet weak var documentImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func scanButton(_ sender: Any) {
+        let scannerViewController = ImageScannerController()
+        scannerViewController.imageScannerDelegate = self
+        present(scannerViewController, animated: true)
+        print("SCAN BUTTON PRESSED!")
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+        // You are responsible for carefully handling the error
+        print(error)
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+        // The user successfully scanned an image, which is available in the ImageScannerResults
+        // You are responsible for dismissing the ImageScannerController
+        scanner.dismiss(animated: true)
+    }
+    
+    func imageScannerControllerDidFinish(_ scanner: ImageScannerController) {
+        scanner.dis
+    }
+    
+    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+        // The user tapped 'Cancel' on the scanner
+        // You are responsible for dismissing the ImageScannerController
+        scanner.dismiss(animated: true)
+    }
+    
+}
+
